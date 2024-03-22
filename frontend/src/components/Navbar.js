0777aca9c4aad11342c1fe5fd83fea252f3f0079
neeru0713 <neerurani1307@gmail.com>
2024-03-22 14:57:54 +0530

    added functionality of logout

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -5,6 +5,12 @@ import { FaUserCircle } from "react-icons/fa";
 
 const Navbar = () => {
     const { user, setUser } = useContext(UserContext); 
+
+
+const logoutHandler = () =>{
+    setUser(null)
+    localStorage.setItem("token", "")
+}
   return (
     <div className="flex bg-blue-700 p-3 justify-between">
       <div className="flex gap-1 text-semibold">
@@ -17,7 +23,13 @@ const Navbar = () => {
    
       </div>
       <div className="text-white text-2xl">Blogs</div>
-      {user && (<div className="flex justify-between items-center gap-1">  <FaUserCircle className="text-white text-xl"/><div className="text-white font-semibold text-xl">{user?.username}</div>
+      {user && (<div className="flex gap-4">  
+      <div className="flex justify-between items-center gap-1">
+      <FaUserCircle className="text-white text-xl"/>
+      <div className="text-white font-semibold text-xl">{user?.username}</div>
+      </div>
+      <button onClick={logoutHandler} className="text-white border rounded-lg py-1 px-2">Logout</button>
+      
      
       </div>)}
       {!user && (<div className="flex gap-1 text-semibold">
